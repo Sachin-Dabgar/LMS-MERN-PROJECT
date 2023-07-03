1cdaf877aa570487d754e0293a7ae511dc7e9919
Sachin-Dabgar <bosscoder1325@gmail.com>
2023-07-03 21:32:43 +0530

    added changes for the dynamic cards in homepage

--- src/components/Core/HomePage/CourseCard.jsx
+++ src/components/Core/HomePage/CourseCard.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 import { FaUserFriends } from "react-icons/fa";
+import { MdClass } from "react-icons/md";
 
 const CourseCard = ({ cardData, currentCard, setCurrentCard }) => {
+    console.log("here is current card", currentCard);
     return (
-        <div className="w-[360px] lg:w-[30%] bg-richblack-800  text-richblack-25 h-[300px] box-border cursor-pointer">
-            <div className="border-b-[2px] border-richblack-400 border-dashed h-[80%] p-6 flex flex-col gap-3">
-                <div className=" false font-semibold text-[20px]">
-                    Learn CSS
+        <div
+            onClick={() => setCurrentCard(cardData.heading)}
+            className={`w-[360px] lg:w-[30%] ${
+                currentCard
+                    ? "bg-white shadow-yellow-50  text-richblack-25 box-border cursor-pointer"
+                    : "bg-richblack-800  text-richblack-25"
+            } shadow-[12px_12px_0_0]  h-[300px] `}
+        >
+            <div
+                className={`border-b-[2px] border-richblack-400 border-dashed h-[80%] p-6 flex flex-col gap-3`}
+            >
+                <div className=" text-richblack-800 font-semibold text-[20px]">
+                    {cardData.heading}
                 </div>
-                <div className="text-richblack-400">
-                    This course explores advanced topics in HTML5 and CSS3,
-                    including animations, transitions, and layout techniques
-                </div>
+                <div className="text-richblack-400">{cardData.description}</div>
             </div>
-            <div className="flex justify-between text-richblack-300 px-6 py-3 font-medium">
+            <div className="flex justify-between text-blue-300 px-6 py-3 font-medium">
                 <div className="flex items-center gap-2 text-[16px]">
                     <FaUserFriends />
-                    <p>Beginner</p>
+                    <p>{cardData.level}</p>
                 </div>
                 <div className="flex items-center gap-2 text-[16px]">
-                    {/* svg */}
-                    <p>6 lessions</p>
+                    <MdClass />
+                    <p>{cardData.lessionNumber} lessions</p>
                 </div>
             </div>
         </div>
--- src/components/Core/HomePage/ExploreMore.jsx
+++ src/components/Core/HomePage/ExploreMore.jsx
@@ -5,10 +5,10 @@ import CourseCard from "./CourseCard.jsx";
 
 const tabsName = [
     "Free",
-    "New to Coding",
-    "Most Popular",
-    "Skill Paths",
-    "Career Paths",
+    "New to coding",
+    "Most popular",
+    "Skills paths",
+    "Career paths",
 ];
 
 const ExploreMore = () => {
@@ -50,20 +50,20 @@ const ExploreMore = () => {
                         {tabname}
                     </div>
                 ))}
-                <div className="hidden lg:block lg:h-[200px]"></div>
-                {/* course cards */}
-                <div className="lg:absolute gap-10 justify-center lg:gap-0 flex lg:justify-between flex-wrap w-full lg:bottom-[0] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[50%] text-black lg:mb-0 mb-7 lg:px-0 px-3">
-                    {courses.map((course, index) => {
-                        return (
-                            <CourseCard
-                                key={index}
-                                cardData={course}
-                                currentCard={currentCard}
-                                setCurrentCard={setCurrentCard}
-                            />
-                        );
-                    })}
-                </div>
+            </div>
+            <div className="hidden lg:block lg:h-[200px]"></div>
+            {/* course cards */}
+            <div className="lg:absolute gap-10 justify-center lg:gap-0 flex lg:justify-between flex-wrap w-full lg:bottom-[0] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[50%] text-black lg:mb-0 mb-7 lg:px-0 px-3">
+                {courses?.map((course, index) => {
+                    return (
+                        <CourseCard
+                            key={index}
+                            cardData={course}
+                            currentCard={currentCard}
+                            setCurrentCard={setCurrentCard}
+                        />
+                    );
+                })}
             </div>
         </div>
     );
